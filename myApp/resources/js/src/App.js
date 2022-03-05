118e6b482ca02187df559559f3998fdf5ac6e7eb
AhmadRyahnah <ahmad.riahna@gmail.com>
2022-03-05 16:50:34 +0300

    first commit

---
+++ myApp/resources/js/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const App =()=>{
+
+
+
+    return(
+        <h1>Ahmad Ryahnah</h1>
+    )
+}
+
+export default App;
---
+++ myApp/resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+
+
+
+    <title>Farms Booking</title>
+    <link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+
+  <body>
+
+    <div id="app"> </div>
+  </body>
+</html>
