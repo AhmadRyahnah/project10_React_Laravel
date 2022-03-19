360977512c3075356cfbb251d361860206ce43a8
AhmadRyahnah <ahmad.riahna@gmail.com>
2022-03-19 05:17:14 +0300

    userEdit

--- myApp/resources/js/src/Components/Profile/Profile.js
+++ myApp/resources/js/src/Components/Profile/Profile.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './Profile.module.css'
 import { AiOutlineSetting, AiOutlineMail, AiOutlineUser } from 'react-icons/ai';
-
+import { BsTelephone } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import Avatar from 'react-avatar';
+import EditProfile from './editProfile'
 const Profile = () => {
+
+
+    const [user, setUser] = useState();
+
+    const [click, setClick] = useState(false)
+
+    useEffect(() => {
+
+        const myUser = localStorage.getItem('loggedUser') ? JSON.parse(localStorage.getItem('loggedUser')) : []
+        setUser(myUser)
+    },[])
+
+    const Settings = () => {
+        setClick(true)
+    }
     const logout = () => {
         localStorage.removeItem('loggedUser')
 
@@ -16,66 +32,74 @@ const Profile = () => {
     }
 
     return (
+
         <div className={styles.profileContianer}><br /><br />
             <div className={styles.profileHeader}>
                 <h1>My Profile</h1>
                 <ul className={styles.listButton}>
-                    <li><button className={styles.btnSetting}>Settings<i><AiOutlineSetting /></i> </button></li>
+                    <li><button className={styles.btnSetting} onClick={Settings} >Settings<i><AiOutlineSetting /></i> </button></li>
                     <li><Link to="/SignInUp" ><button onClick={logout} className={styles.btnLogout}>Logout</button></Link></li>
                 </ul>
-            </div><br />
-            <div className={styles.information}>
-                {/* <h1>My Information</h1> */}
-                <div className={styles.detailes}>
-                    <Avatar round="50%" name="Ahmad Ryahnah" />
-                    <h4><AiOutlineUser /> ahmad</h4>
-                    <h4><i className={styles.icon} >< AiOutlineMail /></i> ahmad@gmail.com</h4>
-                </div>
-            </div>
-            <br />
-            <div className={styles.booking}>
-                <div className={styles.BookingsContaiiner}>
-                    <div className={styles.Bookings}>
-                        <h3>Your Bookings:</h3>
-                    </div>
-                    <div className={styles.customProfile}>
-                        <table >
-                            <tr>
-                                <th><h3>name</h3></th>
-                                <th><h3>name</h3></th>
-                                <th><h3>name</h3></th>
-                                <th><h3>name</h3></th>
-                                <th><h3>name</h3></th>
-                            </tr>
-
-                            <tr>
-
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                            </tr>
-                            <tr>
-
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                            </tr>
-                            <tr>
-
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                                <td><h5>ahmad</h5></td>
-                            </tr>
-                        </table>
+            </div><br /> {!click ? <>
+                <div className={styles.information}>
+                    {/* <h1>My Information</h1> */}
+                    <div className={styles.detailes}>
+                        <Avatar round="50%" name={user ? user.name : null} />
+                        <h4><AiOutlineUser /> {user ? user.name : null}</h4>
+                        <h4><i className={styles.icon} >< AiOutlineMail /></i>{user ? user.email : null}</h4>
+                        <h4><i className={styles.icon} >< BsTelephone /></i> 07777777777</h4>
                     </div>
                 </div>
-            </div>
+                <br />
+
+                <div className={styles.booking}>
+                    <div className={styles.BookingsContaiiner}>
+                        <div className={styles.Bookings}>
+                            <h3>Your Bookings:</h3>
+                        </div>
+                        <div className={styles.customProfile}>
+                            <table >
+                                <tr>
+                                    <th><h3>name</h3></th>
+                                    <th><h3>name</h3></th>
+                                    <th><h3>name</h3></th>
+                                    <th><h3>name</h3></th>
+                                    <th><h3>name</h3></th>
+                                </tr>
+
+                                <tr>
+
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                </tr>
+                                <tr>
+
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                </tr>
+                                <tr>
+
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                    <td><h5>ahmad</h5></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </> : <div className={styles.booking}>
+
+
+                <EditProfile setClick={setClick} setUser={setUser} /></div>}
         </div>
 
     )
--- myApp/resources/js/src/Components/Profile/editProfile.js
+++ myApp/resources/js/src/Components/Profile/editProfile.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import swal from 'sweetalert';
+import '../Admin/User/User.css'
 
-const editProfile = () => {
-  return (
-    <div>editProfile</div>
-  )
+const EditProfile = (props) => {
+
+
+
+
+    const [user, setUser] = useState(localStorage.getItem('loggedUser') ? JSON.parse(localStorage.getItem('loggedUser')) : []);
+
+    useEffect(async () => {
+
+        await axios.get("http://127.0.0.1:8000/api/editShowUser/" + user.id).then((response) => {
+            console.log(response.data);
+            setUser(response.data.user)
+
+        });
+    }, [setUser]);
+
+    if (user)
+        console.log(user);
+
+    let navigate = useNavigate()
+    const [inputs, setInputs] = useState([]);
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setUser(values => ({ ...values, [name]: value }));
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+        axios.put('http://127.0.0.1:8000/api/editUser/' + user.id, user);
+        props.setClick(false)
+        localStorage.setItem('loggedUser', JSON.stringify(user))
+        setUser(localStorage.getItem('loggedUser') ? JSON.parse(localStorage.getItem('loggedUser')) : [])
+        navigate('/Profile')
+
+    }
+
+
+    return (
+        <div >
+
+
+
+            <form onSubmit={handleSubmit} >
+                <label htmlFor="fname">Name</label>
+                <input value={user ? user.name : null} onChange={handleChange} type="text" id="fname" name="name" placeholder="Your name.." />
+
+                <label htmlFor="lname">Email</label>
+                <input value={user ? user.email : null} onChange={handleChange} type="text" id="lname" name="email" placeholder="Your last name.." />
+
+                <label htmlFor="country">Phone</label>
+                <input value={user ? user.phone : null} onChange={handleChange} type="text" id="lname" name="phone" placeholder="Your last name.." />
+
+                {/* <label htmlFor="country">Role</label>
+
+                    <select id="country" name="role" onChange={handleChange}>
+                        <option value={user ? user.role !== 0 ? '1' : '0' : null}>{user ? user.role !== 0 ? 'Admin' : 'User' : null}</option>
+                        <optin value={user ? user.role === 0 ? '1' : '0' : null}>{user ? user.role === 0 ? 'Admin' : 'User' : null}</option>
+
+                    </select> */}
+
+
+
+                <input type="submit" value="Submit" />
+            </form>
+
+        </div>
+
+    )
+
 }
-
-export default editProfile
+export default EditProfile;
